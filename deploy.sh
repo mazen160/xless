b8d3aa7c0aacd4eeed4e68c05168346a413bdf5a
Neolex <hascoet.kevin@neolex-security.fr>
2021-05-26 02:30:21 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-read -p "Enter Slack Incoming Hook: " "slack_incoming_webhook"
-vercel secret add slack-incoming-webhook "$slack_incoming_webhook"
+vercel secret add slack-incoming-webhook "https://hooks.slack.com/services/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"
 
-read -p "Enter IMGBB API Key: " "imgbb_api_key"
-vercel secret add imgbb-api-key "$imgbb_api_key"
+vercel secret add imgbb-api-key "d1e2b459ba603a35da64276cdec91790"
 
 vercel deploy
